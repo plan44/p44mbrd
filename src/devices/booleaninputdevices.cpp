00f761eec6e8080fe2969e76edb7d3fe416f26e3
Lukas Zeller <luz@plan44.ch>
2022-12-19 13:47:45 +0100

    Move focuslog level to 6 (from 5)

--- src/devices/booleaninputdevices.cpp
+++ src/devices/booleaninputdevices.cpp
@@ -25,7 +25,7 @@
 #define ALWAYS_DEBUG 1
 // - set FOCUSLOGLEVEL to non-zero log level (usually, 5,6, or 7==LOG_DEBUG) to get focus (extensive logging) for this file
 //   Note: must be before including "logger.hpp" (or anything that includes "logger.hpp")
-#define FOCUSLOGLEVEL 5
+#define FOCUSLOGLEVEL 6
 
 #include "device_impl.h" // include as first file!
 #include "booleaninputdevices.h"
