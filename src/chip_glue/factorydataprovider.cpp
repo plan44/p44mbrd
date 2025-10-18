916c71f256f31137dcf2b110e343f4702494b225
Lukas Zeller <luz@plan44.ch>
2025-10-18 17:20:17 +0200

    factorydataprovider: dump final factorydata after accumulating from --factorydata specified sources

--- src/chip_glue/factorydataprovider.cpp
+++ src/chip_glue/factorydataprovider.cpp
@@ -78,6 +78,12 @@ FileBasedFactoryDataProvider::FileBasedFactoryDataProvider(const string aFactory
       if (!key.empty()) mDataItems[key] = data; // store last item
     }
   }
+  if (LOGENABLED(LOG_DEBUG)) {
+    LOG(LOG_DEBUG, "\nAccumulated factory data after reading: %s\n", aFactoryDataResourcePaths.c_str());
+    for (DataMap::iterator pos = mDataItems.begin(); pos!=mDataItems.end(); ++pos) {
+      LOG(LOG_DEBUG, "%s=%s", pos->first.c_str(), pos->second.c_str());
+    }
+  }
 }
 
 bool FileBasedFactoryDataProvider::getItem(const char* aKey, string &aItem)
